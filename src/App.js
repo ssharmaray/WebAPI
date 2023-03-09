cf65253d3c0808638b10dbc770c86516a088f0d5
ssharmaray <116895846+ssharmaray@users.noreply.github.com>
2023-03-09 13:21:37 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import logo from './logo.svg';
+import './App.css';
+ //import CRUD from './CRUD';
+import Feedback from './Feedback';
+
+function App() {
+  return (
+    <div className="container">
+        <Feedback/>
+    </div>
+  );
+}
+
+export default App;
